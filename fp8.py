c72d033bf7d7cd81b0ca701cf8af4d57094aea16
Eitan Turok <eitan.turok@gmail.com>
2024-12-12 07:21:12 +0000

    fp8 init

---
+++ fp8.py
@@ -0,0 +1,19 @@
+from tinygrad import Tensor, dtypes
+from tinygrad.device import Device
+from icecream import ic, install
+
+"""
+
+PTX=1 python fp8.py
+t = Tensor([1, 2, 3], dtype=dtypes.fp8_e4m3, device="CUDA")
+KeyError: dtypes.fp8_e4m3
+
+CUDA=1 python fp8.py
+t = Tensor([1, 2, 3], dtype=dtypes.fp8_e4m3, device="CUDA")
+AttributeError: module 'tinygrad.runtime.autogen.nvrtc' has no attribute 'nvrtcVersion'
+"""
+install()
+
+devices = [d for d in Device.get_available_devices()]
+t = Tensor([1, 2, 3], dtype=dtypes.fp8_e4m3, device="CUDA")
+ic(t.realize())
