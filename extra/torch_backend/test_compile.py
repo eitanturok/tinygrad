e6c0a0525998c006f2f698081c51eaa8a4ede8da
Eitan Turok <eitan.turok@gmail.com>
2025-06-19 17:10:17 +0300

    better tests

--- extra/torch_backend/test_compile.py
+++ extra/torch_backend/test_compile.py
@@ -1,5 +1,6 @@
-# https://pytorch.org/tutorials/intermediate/torch_compile_tutorial.html
+# https://docs.pytorch.org/tutorials/intermediate/torch_compile_tutorial.html#torchdynamo-and-fx-graphs
 import torch
+import os, time, unittest
 import torch._dynamo
 from extra.torch_backend.backend import unwrap, wrap
 
@@ -22,15 +23,42 @@ def tiny(gm:torch.fx.GraphModule, sample_inputs):
     return torch_function
   return aot_module_simplified(gm, sample_inputs, decompositions={}, fw_compiler=my_compiler)
 
-if __name__ == "__main__":
-  def foo(x, y):
-    a = torch.sin(x)
-    b = torch.cos(y)
-    return a + b
+class TestTorchCompile(unittest.TestCase):
+  def setUp(self):
+    import tinygrad.frontend.torch
+    os.environ["TORCHDYNAMO_VERBOSE"] = "1"
+    os.environ["TORCH_DEBUG"] = "1"
 
-  print("calling compile")
-  opt_foo1 = torch.compile(foo, backend="tiny")
-  print("compiled")
-  for i in range(5):
-    out = opt_foo1(torch.randn(10, 10), torch.randn(10, 10))
-    print(out.device)
+  def run_fxn(self, f, *args):
+    times = []
+    outputs = []
+    for _ in range(5):
+      st = time.perf_counter_ns()
+      out = f(*args)
+      times.append(time.perf_counter_ns() - st)
+      outputs.append(out)
+    return outputs, times
+
+  def test_torch_compiler_tiny_backend_correctness(self):
+    def fxn(x:torch.Tensor): return x.sum()
+    x = torch.randn(10, 10)
+
+    torch_outputs, _ = self.run_fxn(torch.compile(fxn), x)
+    torch._dynamo.reset() # reset since we are using a different backend # remove?
+    tiny_outputs, _ = self.run_fxn(torch.compile(fxn, backend="tiny"), x.to("tiny"))
+    assert [out.item() for out in torch_outputs] == [out.item() for out in tiny_outputs]
+    assert all([out.device == "tiny" for out in tiny_outputs])
+
+  def test_torch_compiler_tiny_backend_speed(self):
+    def fxn(x:torch.Tensor): return x.sum()
+    x = torch.randn(10, 10)
+
+    _, torch_times = self.run_fxn(torch.compile(fxn), x)
+    torch._dynamo.reset() # reset since we are using a different backend
+    _, tiny_times = self.run_fxn(torch.compile(fxn, backend="tiny"), x.to("tiny"))
+    print(torch_times, tiny_times)
+    assert torch_times[0] == max(torch_times)
+    assert tiny_times[0] == max(tiny_times)
+
+if __name__ == "__main__":
+  unittest.main()
