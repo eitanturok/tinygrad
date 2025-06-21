36fb45287d0b05a7285988b5d1e037f2a4088f61
Eitan Turok <eitan.turok@gmail.com>
2025-06-22 02:35:52 +0300

    better tests

--- extra/torch_backend/test_compile.py
+++ extra/torch_backend/test_compile.py
@@ -9,6 +9,9 @@ from torch._functorch.aot_autograd import aot_module_simplified
 
 from tinygrad import Tensor, TinyJit
 
+from icecream import install
+install()
+
 @register_backend
 def tiny(gm:torch.fx.GraphModule, sample_inputs):
   def my_compiler(gm:torch.fx.GraphModule, sample_inputs):
@@ -19,8 +22,10 @@ def tiny(gm:torch.fx.GraphModule, sample_inputs):
       for x in outs: unwrap(x).realize()
       return outs
     # TODO: this should be able to pass in .tiny() Tensors, not need to convert them. it tries to access Storage if you pass in.
-    def torch_function(*args:torch.Tensor): return tiny_function(*[unwrap(x.tiny()) for x in args])
+    def torch_function(*args:torch.Tensor):
+      return tiny_function(*[unwrap(x.tiny()) for x in args])
     return torch_function
+  gm.graph.print_tabular()
   return aot_module_simplified(gm, sample_inputs, decompositions={}, fw_compiler=my_compiler)
 
 class TestTorchCompile(unittest.TestCase):
@@ -39,24 +44,45 @@ class TestTorchCompile(unittest.TestCase):
       outputs.append(out)
     return outputs, times
 
-  def test_torch_compiler_tiny_backend_correctness(self):
+  def test_correctness(self):
+    import numpy as np
+    def fxn(x:torch.Tensor|np.ndarray) -> torch.Tensor|np.ndarray: return x.sum()
+    t = torch.randn(4, 4)
+
+    ret1 = torch.compile(fxn)(t)
+    torch._dynamo.reset() # remove?
+    ret2 = torch.compile(fxn, backend="tiny")(t)
+    torch._dynamo.reset() # remove?
+    ret3 = torch.compile(fxn, backend="tiny")(t.to("tiny"))
+
+    expected = fxn(t.numpy())
+    assert ret1.numpy() == ret2.numpy() == ret3.numpy() == expected
+
+  def test_correctness_old(self):
     def fxn(x:torch.Tensor): return x.sum()
-    x = torch.randn(10, 10)
+    x = torch.randn(4, 4)
 
+    # tensor backend = torch, compiler backend = torch
     torch_outputs, _ = self.run_fxn(torch.compile(fxn), x)
-    torch._dynamo.reset() # reset since we are using a different backend # remove?
-    tiny_outputs, _ = self.run_fxn(torch.compile(fxn, backend="tiny"), x.to("tiny"))
-    assert [out.item() for out in torch_outputs] == [out.item() for out in tiny_outputs]
-    assert all([out.device == "tiny" for out in tiny_outputs])
 
-  def test_torch_compiler_tiny_backend_speed(self):
+    # tensor backend = torch, compiler backend = tiny
+    torch._dynamo.reset() # remove?
+    tiny_outputs, _ = self.run_fxn(torch.compile(fxn, backend=tiny), x)
+
+    # tensor backend = tiny, compiler backend = tiny
+    torch._dynamo.reset() # remove?
+    tiny_outputs, _ = self.run_fxn(torch.compile(fxn, backend="tiny"), x.to("tiny"))
+
+    # assert
+    assert [out.item() for out in torch_outputs] == [out.item() for out in tiny_outputs]
+
+  def test_speed(self):
     def fxn(x:torch.Tensor): return x.sum()
     x = torch.randn(10, 10)
 
     _, torch_times = self.run_fxn(torch.compile(fxn), x)
     torch._dynamo.reset() # reset since we are using a different backend
     _, tiny_times = self.run_fxn(torch.compile(fxn, backend="tiny"), x.to("tiny"))
-    print(torch_times, tiny_times)
     assert torch_times[0] == max(torch_times)
     assert tiny_times[0] == max(tiny_times)
 
